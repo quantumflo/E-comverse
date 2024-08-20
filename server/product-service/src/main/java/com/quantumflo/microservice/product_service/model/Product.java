4bf1ef0ce4e73f7aef0a5b6dd6252110f99a924e
shashank <shashankhebbarm@gmail.com>
2024-08-20 11:56:09 +0530

    data model

---
+++ server/product-service/src/main/java/com/quantumflo/microservice/product_service/model/Product.java
@@ -0,0 +1,24 @@
+package com.quantumflo.microservice.product_service.model;
+
+import java.math.BigDecimal;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Document(collection = "product")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class Product {
+
+    private String id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    
+}
