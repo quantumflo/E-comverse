405f9106bac6b4ebdde49ad6fd9c6de0e4149661
shashank <shashankhebbarm@gmail.com>
2024-08-25 18:52:45 +0530

    openfeign

--- server/order-service/src/main/java/com/quantumflo/microservices/order_service/service/OrderService.java
+++ server/order-service/src/main/java/com/quantumflo/microservices/order_service/service/OrderService.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
+import com.quantumflo.microservices.order_service.client.InventoryClient;
 import com.quantumflo.microservices.order_service.dtos.OrderRequest;
 import com.quantumflo.microservices.order_service.model.Order;
 import com.quantumflo.microservices.order_service.repository.OrderRepository;
@@ -15,14 +16,20 @@ import lombok.RequiredArgsConstructor;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
     
     public void placeOrder(OrderRequest orderRequest) {
-        Order order = new Order();
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setQuantity(orderRequest.quantity());
-        order.setPrice(orderRequest.price());
-        order.setSkuCode(orderRequest.skuCode());
-        orderRepository.save(order);
 
+        var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
+        if( isProductInStock ) {
+        Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setQuantity(orderRequest.quantity());
+            order.setPrice(orderRequest.price());
+            order.setSkuCode(orderRequest.skuCode());
+            orderRepository.save(order);
+        } else {
+            throw new RuntimeException("Product with skuCode " + orderRequest.skuCode() +"  is out of stock" );
+        }    
     }
 }
