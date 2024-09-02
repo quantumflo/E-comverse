2fd40a0e672265e49e67807fb00358ab4e0c7c5b
shashank <shashankhebbarm@gmail.com>
2024-09-02 21:34:17 +0530

    routes

--- server/api-gateway/src/main/java/com/quantumflo/gateway/api_gateway/routes/Routes.java
+++ server/api-gateway/src/main/java/com/quantumflo/gateway/api_gateway/routes/Routes.java
@@ -13,25 +13,28 @@ import org.springframework.web.servlet.function.ServerResponse;
  */
 @Configuration
 public class Routes {
+    private static final String productServiceURL   = "http://localhost:8080";
+    private static final String orderServiceURL     = "http://localhost:8081";
+    private static final String inventoryServiceURL = "http://localhost:8082";
 
     @Bean
     public RouterFunction<ServerResponse> productServiceRoute() {
         return GatewayRouterFunctions.route("product_service").
-                route(RequestPredicates.path("/api/product"), HandlerFunctions.http("http://localhost:8080"))
+                route(RequestPredicates.path("/api/product"), HandlerFunctions.http(productServiceURL))
                 .build();
     }
 
     @Bean
     public RouterFunction<ServerResponse> orderServiceRoute() {
         return GatewayRouterFunctions.route("order_service").
-                route(RequestPredicates.path("/api/order"), HandlerFunctions.http("http://localhost:8081"))
+                route(RequestPredicates.path("/api/order"), HandlerFunctions.http(orderServiceURL))
                 .build();
     }
     
     @Bean
     public RouterFunction<ServerResponse> inventoryServiceRoute() {
         return GatewayRouterFunctions.route("inventory_service").
-                route(RequestPredicates.path("/api/inventory"), HandlerFunctions.http("http://localhost:8082"))
+                route(RequestPredicates.path("/api/inventory"), HandlerFunctions.http(inventoryServiceURL))
                 .build();
     }
 }
