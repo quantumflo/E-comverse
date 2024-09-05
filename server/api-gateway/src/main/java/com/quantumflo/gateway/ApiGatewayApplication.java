70ae9393974c9be5be5f0622e3746968fbc1b3d0
shashank <shashankhebbarm@gmail.com>
2024-09-05 19:55:34 +0530

    move mainapp outside

--- server/api-gateway/src/main/java/com/quantumflo/gateway/ApiGatewayApplication.java
+++ server/api-gateway/src/main/java/com/quantumflo/gateway/ApiGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.quantumflo.gateway.api_gateway;
+package com.quantumflo.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
