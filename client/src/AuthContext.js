fbda41096a350f958c052ca570091f5558766ecd
shashank <shashankhebbarm@gmail.com>
2024-09-09 13:16:15 +0530

    token context

---
+++ client/src/AuthContext.js
@@ -0,0 +1,36 @@
+import React, { createContext, useEffect, useState } from "react";
+import keycloak from "./config/KeyCloak"; // Ensure the correct path to Keycloak configuration
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const refreshToken = () => {
+      keycloak
+        .updateToken(30)
+        .then((refreshed) => {
+          if (refreshed) {
+            setToken(keycloak.token);
+            console.log("Token refreshed");
+          } else {
+            console.warn(
+              "Token not refreshed, valid for " +
+                Math.round(
+                  keycloak.tokenParsed.exp + keycloak.timeSkew - new Date().getTime() / 1000
+                ) +
+                " seconds"
+            );
+          }
+        })
+        .catch((error) => {
+          console.error("Failed to refresh token", error);
+        });
+    };
+
+    keycloak.onTokenExpired = refreshToken;
+  }, []);
+
+  return <AuthContext.Provider value={{ token, setToken }}>{children}</AuthContext.Provider>;
+};
