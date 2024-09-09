5b2f418d252050f91cd791f076775a71cdbafc8d
shashank <shashankhebbarm@gmail.com>
2024-09-09 13:14:37 +0530

    bring back strictmode

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { AuthProvider } from "./AuthContext"; // Import AuthProvider and AuthContext
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  // <React.StrictMode>
-  <App />
-  // </React.StrictMode>
+  <React.StrictMode>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </React.StrictMode>
 );
