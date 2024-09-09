f3e55e197921bba1f3193bba20e7aeadde3dcc9a
shashank <shashankhebbarm@gmail.com>
2024-09-09 13:14:21 +0530

    add cors

--- server/api-gateway/src/main/java/com/quantumflo/gateway/config/SecurityConfig.java
+++ server/api-gateway/src/main/java/com/quantumflo/gateway/config/SecurityConfig.java
@@ -1,26 +1,38 @@
 package com.quantumflo.gateway.config;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-
-// The method configures a security filter chain that requires all HTTP requests to be authenticated and 
-// sets up the application as an OAuth2 resource server using JWT for authentication.
 @Configuration
 public class SecurityConfig {
 
     private final String[] allowedUrls = new String[] {"/swagger-ui.html", "/v3/api-docs/**", "/api-docs/**", "/swagger-ui/**", "/aggregate/**"};
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
+            .cors(cors -> cors.configurationSource(corsConfigurationSource())) // Add CORS configuration
             .authorizeHttpRequests(authorizeRequests -> authorizeRequests
-            .requestMatchers(null, allowedUrls).permitAll()
-            .anyRequest().authenticated())
-            .oauth2ResourceServer(oauth2ResourceServer -> oauth2ResourceServer.jwt(Customizer.withDefaults())
-            ).build();
+                .requestMatchers(null, allowedUrls).permitAll()
+                .anyRequest().authenticated())
+            .oauth2ResourceServer(oauth2ResourceServer -> oauth2ResourceServer.jwt(Customizer.withDefaults()))
+            .build();
     }
-    
+
+    @Bean
+    public UrlBasedCorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("http://localhost:3000"); // Allow specific origin
+        configuration.addAllowedMethod("*"); // Allow all HTTP methods
+        configuration.addAllowedHeader("*"); // Allow all headers
+        configuration.setAllowCredentials(true); // Allow credentials
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 }
